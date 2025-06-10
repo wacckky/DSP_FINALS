4607ce8bf018b24eb7422679fd81c9094a842a31
wacckky <joaquin19olarte@gmail.com>
2025-06-11 01:47:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit.components.v1 import html
 
 st.set_page_config(page_title="Mic dB Level", layout="centered")
-st.title("Live Microphone dB Meter")
+st.title("Sound Level Meter")
 st.write("This uses your **browser mic**. Please grant microphone permission when prompted.")
 
 meter_html = """
