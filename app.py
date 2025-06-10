1d1195c2f578903f15f1a324fa8a95dc854b0bba
wacckky <joaquin19olarte@gmail.com>
2025-06-10 22:57:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ meter_html = """
   /* Reset and base */
   html, body {
     margin: 0; padding: 0;
-    background: #ffffff;
+    background: transparent;
     font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     color: #6b7280;
