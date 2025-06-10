a2925684ba806fe5d93382f34480d4d8a227cf62
wacckky <joaquin19olarte@gmail.com>
2025-06-10 16:53:31 +0800

    Delete mic_component/frontend/__init__.py

--- mic_component/frontend/__init__.py
+++
@@ -1,9 +0,0 @@
-import streamlit.components.v1 as components
-
-_component_func = components.declare_component(
-    "mic_db_component",
-    path="./mic_component/frontend",
-)
-
-def mic_db_component():
-    return _component_func(default=0.0)
