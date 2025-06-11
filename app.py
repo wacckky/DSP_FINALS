187e38350c4fcfb2f43b7a2d55ba9ca5d770ba37
wacckky <joaquin19olarte@gmail.com>
2025-06-12 01:50:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ meter_html = """
 
   <div id="app-container">
     <div id="labels">
-      <div class="label red"></span>130</div><span class="tick">
+      <div class="label red"><span class="tick"></span>130</div>
       <div class="label red"><span class="tick"></span>120</div>
       <div class="label red"><span class="tick"></span>110</div>
       <div class="label yellow"><span class="tick"></span>100</div>
