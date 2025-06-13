e34d952d00426e727ef2cede8cb779f49ee4d32e
wacckky <joaquin19olarte@gmail.com>
2025-06-13 14:25:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -262,7 +262,7 @@ function initMic() {
         const rms = Math.sqrt(sumSquares / dataArray.length);
         const reference = 0.05;
         let db = 20 * Math.log10(rms / reference + 1e-6);
-        let positiveDb = Math.max(0, Math.min(130, db + 30));
+        let positiveDb = Math.max(0, Math.min(130, db + 25));
 
 
         const smoothedDb = smoothingFactor * lastDb + (1 - smoothingFactor) * positiveDb;
